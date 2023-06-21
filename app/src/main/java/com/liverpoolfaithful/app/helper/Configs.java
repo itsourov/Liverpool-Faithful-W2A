393478033fe59e8e189a18d18333c49bb1df7df0
Sourov Biswas <itsourov12@gmail.com>
2023-06-21 19:14:37 +0600

    upgraded gradle

--- app/src/main/java/com/liverpoolfaithful/app/helper/Configs.java
+++ app/src/main/java/com/liverpoolfaithful/app/helper/Configs.java
@@ -1,10 +1,10 @@
 package com.liverpoolfaithful.app.helper;
 
 public class Configs {
-    public static final boolean shouldCacheRequest = false;
+    public static final boolean shouldCacheRequest = true;
     public static final boolean homeTabSwipeable  = false;
     public static final boolean replyInCommentEnabled  = false;
-    public static final boolean postDetailsNoFM = false;
+    public static final boolean postDetailsNoFM = true;
     public static boolean showInterstitialAds = false;
     public static boolean showBannerAds = false;
     public static boolean showNativeAds = false;
